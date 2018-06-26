2184a4922fdcb68e5864b4dfadaf610510666ffe
deep0421 <39827541+deep0421@users.noreply.github.com>
2018-06-26 14:10:04 +0530

    Update Product_of_all_other_numbers.py

--- week-1/day-1/Product_of_all_other_numbers.py
+++ week-1/day-1/Product_of_all_other_numbers.py
@@ -17,3 +17,13 @@ def get_products_of_all_ints_except_at_index(int_list):
        
 
    return res
+
+
+
+TESTCASES
+
+print(get_products_of_all_ints_except_at_index([1, 2, 3]))                 expected = [6, 3, 2]
+print(get_products_of_all_ints_except_at_index([8, 2, 4, 3, 1, 5]))        expected = [120, 480, 240, 320, 960, 192]
+print(get_products_of_all_ints_except_at_index([6, 2, 0, 3]))              expected = [0, 0, 36, 0]
+print(get_products_of_all_ints_except_at_index([4, 0, 9, 1, 0]))           expected = [0, 0, 0, 0, 0]
+print(get_products_of_all_ints_except_at_index([-3, 8, 4]))                expected = [32, -12, -24]
