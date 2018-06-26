059b50088aa8a16a55012be4412c52e388c6aeb9
deep0421 <39827541+deep0421@users.noreply.github.com>
2018-06-26 13:03:32 +0530

    Update Highest_product_of_3.py

--- week-1/day-1/Highest_product_of_3.py
+++ week-1/day-1/Highest_product_of_3.py
@@ -14,3 +14,10 @@ def highest_product_of_3(list_of_ints):
    	return b
 
       
+TESTCASES
+
+print(highest_product_of_3([1, 2, 3, 4]))              expected = 24
+print(highest_product_of_3([6, 1, 3, 5, 7, 8, 2]))     expected = 336
+print(highest_product_of_3([-5, 4, 8, 2, 3]))          expected = 96
+print(highest_product_of_3([-10, 1, 3, 2, -10]))       expected = 300
+print(highest_product_of_3([-5, -1, -3, -2]))          expected = -6
